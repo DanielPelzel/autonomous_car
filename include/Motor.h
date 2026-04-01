638727304f77ba6d581d70511466b74fac6bb243
Daniel Pelzel <pelzel.daniel@gmail.com>
2026-04-01 13:39:37 +0200

    added documantation

--- include/Motor.h
+++ include/Motor.h
@@ -1,17 +1,48 @@
 #pragma once
 #include <cstdint>
 
+
+/**
+ * @brief Controls Dc Motor via L9110S Driver
+ *
+ * Uses two PWM signals (forward / backwards) to control the Motor
+ */
 class Motor {
 private:
-    uint_8_t pinA; //insert pin numbers
+    //Jeder Motor hat eigene Pins
+    uint8_t pinA; //insert pin numbers
     uint8_t pinB; //insert pin numbers
     uint8_t channel1; //insert Channel number
-    uint8_t channel2; //insert channel number 
+    uint8_t channel2; //insert channel number
+
+    static const uint16_t PWM_FREQUENCY = 5000;
+    static const uint8_t PWM_Bits = 8;
 
 
 public:
-    Motor(int pinA, int pinB);
+    /**
+     *
+     * @param pinA GPIO Pin forward
+     * @param pinB GPIO Pin backward
+     * @param channel1 PWM Channel for pinA
+     * @param channel2 PWM Channel for pinB
+     */
+    Motor(uint8_t pinA, uint8_t pinB, uint8_t channel1, uint8_t channel2);
+
+    /**
+     * @brief car drives forward
+     * @param speed_percent speed in percent (0-100)
+     */
     void forward(int speed_percent);
+
+    /**
+     * @brief car drives backward
+     * @param speed_percent speed in percent (0-100)
+     */
     void backward(int speed_percent);
+
+    /**
+     * @brief car stops
+     */
     void stop();
 };
--- src/Motor.cpp
+++ src/Motor.cpp
@@ -11,20 +11,22 @@ Motor::Motor(uint8_t pinA, uint8_t pinB, uint8_t channel1, uint8_t channel2) {
     this->channel1 = channel1;
     this->channel2 = channel2;
 
-    ledcSetup(channel1, 5000, 8);
+    ledcSetup(channel1, PWM_FREQUENCY, PWM_Bits);
     ledcAttachPin(pinA, channel1);
 
-    ledcSetup(channel2, 5000, 8);
+    ledcSetup(channel2, PWM_FREQUENCY,PWM_Bits);
     ledcAttachPin(pinB, channel2);
 }
 
 void Motor::forward(int speed_percent) {
+    //Prrozent auf PWM umrechnen
     int speed_PWM = map(speed_percent, 0, 100, 0, 255);
     ledcWrite(channel1, speed_PWM) ;
     ledcWrite(channel2, 0);
 }
 
 void Motor::backward(int speed_percent) {
+    //Prozent auf PWM umrechnen
     int speed_PWM = map(speed_percent, 0, 100, 0, 255);
     ledcWrite(channel1, 0);
     ledcWrite(channel2, speed_PWM);
